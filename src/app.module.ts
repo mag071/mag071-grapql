6b3c98e3d40426453313915c5faad075fb52dda1
Mario González <mag071@gmail.com>
2022-10-31 15:15:23 -0400

    Primer Commit

---
+++ src/app.module.ts
@@ -0,0 +1,103 @@
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+import { ItemsModule } from './items/items.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { SeedModule } from './seed/seed.module';
+import { CommonModule } from './common/common.module';
+import { ListsModule } from './lists/lists.module';
+import { ListItemModule } from './list-item/list-item.module';
+
+
+@Module({
+  imports: [
+
+    ConfigModule.forRoot(),
+
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ AuthModule ],
+      inject: [ JwtService ],
+      useFactory: async( jwtService: JwtService ) => ({
+        playground: false,
+        autoSchemaFile: join( process.cwd(), 'src/schema.gql'), 
+        plugins: [
+          ApolloServerPluginLandingPageLocalDefault
+        ],
+        context({ req }) {
+          // const token = req.headers.authorization?.replace('Bearer ','');
+          // if ( !token ) throw Error('Token needed');
+
+          // const payload = jwtService.decode( token );
+          // if ( !payload ) throw Error('Token not valid');
+          
+        }
+      })
+    }),
+
+    // TODO: configuración básica
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   // debug: false,
+    //   playground: false,
+    //   autoSchemaFile: join( process.cwd(), 'src/schema.gql'), 
+    //   plugins: [
+    //     ApolloServerPluginLandingPageLocalDefault
+    //   ]
+    // }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      ssl: ( process.env.STATE === 'prod')
+        ? {
+          rejectUnauthorized: false,
+          sslmode: 'require',
+        } 
+        : false as any,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
+    ItemsModule,
+
+    UsersModule,
+
+    AuthModule,
+
+    SeedModule,
+
+    CommonModule,
+
+    ListsModule,
+
+    ListItemModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+   constructor() {
+    console.log("STATE", process.env.STATE);
+    console.log("host", process.env.DB_HOST);
+    console.log("port", +process.env.DB_PORT);
+    console.log("username", process.env.DB_USERNAME);
+    console.log("password", process.env.DB_PASSWORD);
+    console.log("database", process.env.DB_NAME);
+
+   }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // forbidNonWhitelisted: true, 
+    })
+  );
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen( PORT );
+
+  console.log(`App Running on port ${ PORT }`);
+  
+}
+bootstrap();
